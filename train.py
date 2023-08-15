e233a049173daf3727f581e09a0346ae36d3385c
sumanpahari <sumanpahari365@gmail.com>
2023-08-15 12:38:51 +0530

    change sklearn lib

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-import pandas as pd 
-from sklearn.model_selection import train_test_split
+import pandas as pd
+from scikit-learn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
